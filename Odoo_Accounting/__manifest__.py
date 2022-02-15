f58992f4cb20d9484b9d2f1e04af6318b57658d3
Hinamizawa <themoonbunnys@gmail.com>
2022-02-15 14:05:26 -0400

    first upload

---
+++ Odoo_Accounting/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Accounting',
+
+    'summary': """App to do simple accounting """,
+
+    'description': """
+        Accounting Module
+        - Profit and Loss
+        - Multiple Categories of product
+        - Interactive
+    
+    """,
+    'author': 'Hinamizawa',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Training',
+
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+
+
+    ],
+    'demo': [
+
+
+    ],
+
+
+
+
+}
